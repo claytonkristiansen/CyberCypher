7c0b5a0f7b248f5aeb54f88faa81df20bf84f465
clayton.kristiansen@hotmail.com <clayton.kristiansen@hotmail.com>
2020-09-05 17:26:07 -0500

    donkey

--- app/src/main/java/com/example/cybercypher/MainActivity.java
+++ app/src/main/java/com/example/cybercypher/MainActivity.java
@@ -4,19 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
 {
+
+
+    Integer i = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //((Button)findViewById(R.id.button)).setText("DONKEY");
     }
 
     public void OpenEncrypt(View view)
     {
-
+        ((Button)findViewById(R.id.button)).setText(i.toString());
+        i++;
     }
 
 
